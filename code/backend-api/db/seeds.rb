76384b400d35c328937271905b3d8cb719f79a77
Aaron Weiss <weissa7@tcnj.edu>
2017-12-01 04:15:57 -0500

    Favicon & website title updated.

--- code/backend-api/db/seeds.rb
+++ code/backend-api/db/seeds.rb
@@ -1,3 +1,13 @@
+# Name: Aaron Weiss
+# Course: CSC-415
+# Semester: Fall 2017
+# Instructor: Dr. Pulimood
+# Project Name: TCNJ-Speaks
+# Project Description: Bring awareness to community about relevant social justice issues.
+# File Name: 20171201010647_create_downvotes.rb
+# File Description: Prep database with default values.
+# Last Modified: 12/01/17 (m/d/y)
+#
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
